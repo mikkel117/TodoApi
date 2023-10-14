ea9d985eeec1101f64163bb510fceb4629255ec5
rumbo <rumbo.9370@gmail.com>
2023-10-14 22:43:42 +0200

    update

---
+++ Models/TodoContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.model
+{
+    public class TodoContext: DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<TodoItem> TodoItems { get; set; } = null!;
+
+    }
+}
